1c79048635eb1a1a438d9c55458f5d7d68de77a3
Peter Mickle <robloxpro408@gmail.com>
2025-02-12 10:19:10 -0600

    Initial Commit

---
+++ bananaClickerFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bananaClickerFirebase
+//
+//  Created by Peter Mickle on 2/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ bananaClickerFirebaseTests/bananaClickerFirebaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  bananaClickerFirebaseTests.swift
+//  bananaClickerFirebaseTests
+//
+//  Created by Peter Mickle on 2/12/25.
+//
+
+import Testing
+@testable import bananaClickerFirebase
+
+struct bananaClickerFirebaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
